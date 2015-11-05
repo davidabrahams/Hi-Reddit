09533b3175ae37173cea68625ba7f3cb33513ee3
YuzhongHuang <yuzhong.huang@students.olin.edu>
2015-11-05 15:33:46 -0500

    test add function and passed

--- app/src/test/java/com/mobileproto/hireddit/hireddit/LinkedListTest.java
+++ app/src/test/java/com/mobileproto/hireddit/hireddit/LinkedListTest.java
@@ -1,11 +1,7 @@
 package com.mobileproto.hireddit.hireddit;
 
-import com.mobileproto.hireddit.hireddit.LinkedList;
-
 import org.junit.Test;
-
 import java.lang.String;
-
 import static org.junit.Assert.*;
 
 /**
@@ -14,13 +10,16 @@ import static org.junit.Assert.*;
 public class LinkedListTest {
     @Test
     public void initialize_CorrectValue_ReturnsTrue() {
-        final String TEST = "test";
+        final String TEST = "test1";
         LinkedList test_node = new LinkedList(TEST, null);
         assertTrue(test_node.m_value.equals(TEST));
     }
 
     @Test
     public void add_CorrectNode_ReturnsTrue() {
-        
+        final String TEST = "test2";
+        LinkedList test_node_head = new LinkedList(TEST, null);
+        test_node_head.add(TEST);
+        assertTrue(test_node_head.m_next.m_value.equals(TEST));
     }
 }
