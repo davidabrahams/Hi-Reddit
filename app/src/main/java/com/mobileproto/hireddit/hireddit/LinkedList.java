2edb2fab6470bda303c180561587b98a831de157
YuzhongHuang <yuzhong.huang@students.olin.edu>
2015-11-05 15:06:12 -0500

    setup test framework

---
+++ app/src/main/java/com/mobileproto/hireddit/hireddit/LinkedList.java
@@ -0,0 +1,11 @@
+package com.mobileproto.hireddit.hireddit;
+
+/**
+ * Created by yhuang on 11/5/2015.
+ */
+public class LinkedList {
+
+    public LinkedList(String value, LinkedList next) {
+        
+    }
+}
