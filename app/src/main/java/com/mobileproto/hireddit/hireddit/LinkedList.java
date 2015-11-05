df4e77830d1337f21278c5f893bfc006fbdfba94
YuzhongHuang <yuzhong.huang@students.olin.edu>
2015-11-05 15:45:04 -0500

    add comments in LinkedList.java

--- app/src/main/java/com/mobileproto/hireddit/hireddit/LinkedList.java
+++ app/src/main/java/com/mobileproto/hireddit/hireddit/LinkedList.java
@@ -1,18 +1,26 @@
 package com.mobileproto.hireddit.hireddit;
 
 /**
- * Created by yhuang on 11/5/2015.
+ * A simple LinkedList class implementation
  */
 public class LinkedList {
 
-    public String m_value;
-    public LinkedList m_next;
+    public String m_value; // String value inside each node
+    public LinkedList m_next; // pointer to the next node
 
+    /**
+     * LinkedList constructor creates a LinkedList object by assigning
+     * a string and another LinkedList object
+     */
     public LinkedList(String value, LinkedList next) {
         this.m_value = value;
         this.m_next = next;
     }
 
+    /**
+     * LinkedList add() method add a new LinkedList object to the current
+     * node by assigning a string to the new node
+     */
     public void add(String value) {
         this.m_next = new LinkedList(value, null);
     }
