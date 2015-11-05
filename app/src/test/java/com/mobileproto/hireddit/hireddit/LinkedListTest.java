921e3d872861d805f4d8a4659a5936bc84e0b67d
YuzhongHuang <yuzhong.huang@students.olin.edu>
2015-11-05 15:29:41 -0500

    successfully build the test and pass the test

--- app/src/test/java/com/mobileproto/hireddit/hireddit/LinkedListTest.java
+++ app/src/test/java/com/mobileproto/hireddit/hireddit/LinkedListTest.java
@@ -11,13 +11,16 @@ import static org.junit.Assert.*;
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
-public class LinkedListTest
-{
+public class LinkedListTest {
     @Test
-    public void initialize_CorrectValue_ReturnsTrue()
-    {
+    public void initialize_CorrectValue_ReturnsTrue() {
         final String TEST = "test";
-        LinkedList test_node = new LinkedList(TEST);
-        assertTrue(test_node.m_value.isEqual(TEST));
+        LinkedList test_node = new LinkedList(TEST, null);
+        assertTrue(test_node.m_value.equals(TEST));
+    }
+
+    @Test
+    public void add_CorrectNode_ReturnsTrue() {
+        
     }
 }
