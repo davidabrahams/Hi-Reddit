45ca598febd0476b2b00a478fbe4a8baee8c38ce
YuzhongHuang <yuzhong.huang@students.olin.edu>
2015-11-07 13:45:42 -0500

    fix file name conflict and finish the WordToSpeech.java implementation

--- app/src/main/java/com/mobileproto/hireddit/hireddit/TextToSpeech.java
+++
@@ -1,9 +0,0 @@
-package com.mobileproto.hireddit.hireddit;
-
-/**
- * TextToSpeech includes a method which will convert text to speech
- * TextToSpeech constructor gets the application context and a String to generate an audio output
- * TextToSpeech is able to speak out different languages, but for this project, we set it to English
- */
-public class TextToSpeech {
-}
---
+++ app/src/main/java/com/mobileproto/hireddit/hireddit/WordToSpeech.java
@@ -0,0 +1,32 @@
+package com.mobileproto.hireddit.hireddit;
+
+import android.content.Context;
+import android.speech.tts.TextToSpeech;
+
+import java.util.Locale;
+
+/**
+ * TextToSpeech includes a method which will convert text to corresponding audio output
+ * TextToSpeech constructor gets the application context
+ * Speak() takes a String to generate an audio output
+ * TextToSpeech is able to speak out different languages, but for this project, we set it to English
+ */
+public class WordToSpeech {
+    private TextToSpeech m_ttobj;
+
+    public WordToSpeech(Context appContext) {
+
+        m_ttobj = new TextToSpeech(appContext, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    m_ttobj.setLanguage(Locale.UK);
+                }
+            }
+        });
+    }
+
+    public void speak(String toSpeak) {
+        m_ttobj.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+    }
+}
