06d564e8024fa5e513a0dbeb619d21b18dcdfaf4
David Abrahams <david.abrahams@students.olin.edu>
2015-11-10 02:44:21 -0500

    added fragment placeholder text and fragment manager

--- app/src/main/java/com/mobileproto/hireddit/hireddit/MainActivity.java
+++ app/src/main/java/com/mobileproto/hireddit/hireddit/MainActivity.java
@@ -1,40 +1,28 @@
 package com.mobileproto.hireddit.hireddit;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import butterknife.ButterKnife;
-
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements MainFragment.OnFragmentInteractionListener
 {
+
+    FragmentManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        manager = getSupportFragmentManager();
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+        switchFragment(MainFragment.newInstance());
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-//        THIS CODE CAME WITH THE PRE-BUILT ACTIVITY BUT I DON'T THINK WE HAVE A USE FOR IT RIGHT NOW
-
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener()
-//        {
-//            @Override
-//            public void onClick(View view)
-//            {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
     }
 
     @Override
@@ -59,5 +47,12 @@ public class MainActivity extends AppCompatActivity
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void switchFragment(Fragment f)
+    {
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.container, f).addToBackStack(null);
+        transaction.commit();
     }
 }
--- app/src/main/java/com/mobileproto/hireddit/hireddit/MainFragment.java
+++ app/src/main/java/com/mobileproto/hireddit/hireddit/MainFragment.java
@@ -1,8 +1,9 @@
 package com.mobileproto.hireddit.hireddit;
 
-import android.support.v4.app.Fragment;
+
+import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,15 +13,45 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 /**
- * A placeholder fragment containing a simple view.
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MainFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MainFragment#newInstance} factory method to
+ * create an instance of this fragment.
  */
 public class MainFragment extends Fragment
 {
 
+    private OnFragmentInteractionListener mListener;
+
     @Bind(R.id.alienImage) ImageView alienImage;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment MainFragment.
+     */
+    public static MainFragment newInstance()
+    {
+        MainFragment fragment = new MainFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     public MainFragment()
     {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
     }
 
     @Override
@@ -32,4 +63,39 @@ public class MainFragment extends Fragment
         alienImage.setImageResource(R.drawable.reddit_alien);
         return view;
     }
+
+    @Override
+    public void onAttach(Activity activity)
+    {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach()
+    {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener
+    {
+        // public void onFragmentInteraction(Object o);
+    }
+
 }
