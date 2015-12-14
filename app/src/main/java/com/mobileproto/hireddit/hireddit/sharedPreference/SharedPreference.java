ec687cc6a5a768352d158161e6f66e6dd50bc599
YuzhongHuang <yuzhong.huang@students.olin.edu>
2015-12-13 20:32:24 -0500

    modify the sharedPreference class to make declaration and initialization same line

--- app/src/main/java/com/mobileproto/hireddit/hireddit/sharedPreference/SharedPreference.java
+++ app/src/main/java/com/mobileproto/hireddit/hireddit/sharedPreference/SharedPreference.java
@@ -6,51 +6,35 @@ import android.content.SharedPreferences.Editor;
 
 public class SharedPreference {
 
-    public static final String PREFS_NAME = "HI_REDDIT_PREFS";
+    private static final String PREFS_NAME = "HI_REDDIT_PREFS";
 
     public SharedPreference() {
         super();
     }
 
     public void save(Context context, String key, Boolean text) {
-        SharedPreferences settings;
-        Editor editor;
-
-        settings = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-        editor = settings.edit();
-
+        SharedPreferences settings = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        Editor editor = settings.edit();
         editor.putBoolean(key, text);
-
         editor.apply();
     }
 
     public Boolean getValue(Context context, String key) {
-        SharedPreferences settings;
-        Boolean text;
-
-        settings = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-        text = settings.getBoolean(key, false);
+        SharedPreferences settings = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        Boolean text = settings.getBoolean(key, false);
         return text;
     }
 
     public void clearSharedPreference(Context context) {
-        SharedPreferences settings;
-        Editor editor;
-
-        settings = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-        editor = settings.edit();
-
+        SharedPreferences settings = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        Editor editor = settings.edit();
         editor.clear();
         editor.apply();
     }
 
     public void removeValue(Context context, String key) {
-        SharedPreferences settings;
-        Editor editor;
-
-        settings = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-        editor = settings.edit();
-
+        SharedPreferences settings = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        Editor editor = settings.edit();
         editor.remove(key);
         editor.apply();
     }
